d9c8814a1ead65818ed9206fe3be17a131272ed1
kkeilson <>
2019-12-05 09:30:21 -0500

    Removed testing code

--- loginhook.sh
+++ loginhook.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-# Untar Library folder
-# Used as a login hook
+# Untar Library Folder
 # Created by: KKEILSON
-# Date modified: 20191204
+# Date modified: 20191205
+##############
 ### Change Log
+###
+### 20191204 - Initial commit
+### 20191205 - Updated script to remove testing variables.
+###            Tested and confirmed script is untaring under
+###            user's home directory.
+##############
+
 
 #### Variables
 flagdate="20191204"
 dirTemplate="/Library/.Management/mac_template_2019.tar"
 
-# Get User's Home Directory
-dirHome=$HOME
+### Functions
 
-# Define flag
-fileFlag="$dirHome/.flags/Library.$flagdate"
-
-# Get UID  and GID of console user
-#uidNumber=$(id -u "$USER")
-#username=$(id -u -n "$USER")
-username=$USER
-#gidNumber=$(id -g "$USER")
-
-# Run as console user
-suExec="/usr/bin/su $username -c"
-
-homespaceCheck(){
+homespaceCheck() {
   # Set up environment for Admin and Alumni users with temporary local home directory
-  if [[ $HOME =~ /Users/ ]]; then
+  if [[ $dirHome =~ /Users/ ]]; then
     #Remove NetworkHome link
-    $suExec "/bin/chmod 700 $HOME/Desktop"
-    $suExec "/bin/rm $HOME/Desktop/$username"
-    $suExec "/bin/chmod 500 $HOME/Desktop"
+    $suExec "/bin/chmod 700 $dirHome/Desktop"
+    $suExec "/bin/rm $dirHome/Desktop/$username"
+    $suExec "/bin/chmod 500 $dirHome/Desktop"
     /usr/bin/open /Library/.Management/LocalAlert.app
     exit 0
   fi
@@ -50,10 +44,52 @@ createUserHomeDirectories() {
   done
 }
 
-createLibrary(){
+createLibrary() {
   $suExec "/usr/bin/tar xzpf $dirTemplate -C $dirHome --exclude='._*'"
   $suExec "/usr/bin/touch $fileFlag"
 }
+
+createDesktop() {
+  # Copy Home icon to user's Desktop if it does not exist. Then we change permissions so they cannot delete the link or save to the Desktop Directory
+  # First we check to see if Desktop Directory exists
+
+  if [ ! -d "$dirHome/Desktop" ]; then
+    # If it does not, create it
+    $suExec "/bin/mkdir $dirHome/Desktop"
+  else
+    # If it does, make it writeable
+    $suExec "/bin/chmod -R 755 $dirHome/Desktop"
+  fi
+}
+
+createNetworkHome() {
+  # Check for link on Desktop to Network Home
+  if [ ! -L "$dirHome"/Desktop/"$username" ]; then
+    # Create it if it does not exist
+    $suExec "/bin/ln -s $dirHome $dirHome/Desktop"
+  fi
+  $suExec "/bin/chmod 500 $dirHome/Desktop"
+}
+
+##############################################
+# NO CONFIGURATION NECESSARY BELOW THIS LINE #
+##############################################
+
+# Get UID  and GID of console user
+sleep 5
+username=$(logname)
+
+# Run as console user
+suExec="/usr/bin/su $username -c"
+
+# Get User's Home Directory
+dirHome=$($suExec "echo ~")
+
+# Define library flag location
+fileFlag="$dirHome/.flags/Library.$flagdate"
+
+# Check if user is logged in as local or network account
+homespaceCheck
 
 # Check for existing Library Flag
 if [ ! -f "$fileFlag" ]; then
@@ -62,24 +98,12 @@ if [ ! -f "$fileFlag" ]; then
     createLibrary
     $suExec "/bin/rm $dirHome/._Library"
     $suExec "/bin/chmod -R 755 $dirHome/.flags"
-else
-    echo "$FILE does exist"
 fi
 
-# Copy Home icon to user's Desktop if it does not exist. Then we change permissions so they cannot delete the link or save to the Desktop Directory
-# First we check to see if Desktop Directory exists
+# Create Desktop folder
+createDesktop
 
-if [ ! -d "$dirHome/Desktop" ]; then
-  # If it does not, create it
-  $suExec "/bin/mkdir $dirHome/Desktop"
-else
-  # If it does, make it writeable
-  $suExec "/bin/chmod -R 755 $dirHome/Desktop"
-fi
+# Create Network Home icon
+createNetworkHome
 
-# Check for link on Desktop to Network Home
-if [ ! -L "$HOME"/Desktop/"$username" ]; then
-  # Create it if it does not exist
-  $suExec "/bin/ln -s $HOME $HOME/Desktop"
-fi
-$suExec "/bin/chmod 500 $HOME/Desktop"
+exit 0
